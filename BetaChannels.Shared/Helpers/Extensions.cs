ef1d61fa2814fcccee72e113349f15c2a6c4b632
Milad Xandi <miladxandi@outlook.com>
2026-09-16 21:23:45 +0330

    v1.1.0: تقویم شمسی، اعداد فارسی، رنگ ذغالی، داشبورد جدید

--- BetaChannels.Shared/Helpers/Extensions.cs
+++ BetaChannels.Shared/Helpers/Extensions.cs
@@ -1,9 +1,65 @@
+using System.Globalization;
 using BetaChannels.Shared.Enums;
 
 namespace BetaChannels.Shared.Helpers;
 
 public static class EnumExtensions
 {
+    private static readonly PersianCalendar _persianCalendar = new();
+
+    private static readonly string[] PersianMonthNames =
+    {
+        "فروردین", "اردیبهشت", "خرداد",
+        "تیر", "مرداد", "شهریور",
+        "مهر", "آبان", "آذر",
+        "دی", "بهمن", "اسفند"
+    };
+
+    public static string ToShamsiDate(this DateTime date)
+    {
+        var year = _persianCalendar.GetYear(date);
+        var month = _persianCalendar.GetMonth(date);
+        var day = _persianCalendar.GetDayOfMonth(date);
+        return $"{year}/{month:D2}/{day:D2}".ToPersianDigits();
+    }
+
+    public static string ToShamsiDateLong(this DateTime date)
+    {
+        var year = _persianCalendar.GetYear(date);
+        var month = _persianCalendar.GetMonth(date);
+        var day = _persianCalendar.GetDayOfMonth(date);
+        return $"{day.ToPersianDigits()} {PersianMonthNames[month - 1]} {year.ToPersianDigits()}";
+    }
+
+    public static int GetShamsiYear(this DateTime date) => _persianCalendar.GetYear(date);
+
+    public static int GetShamsiMonth(this DateTime date) => _persianCalendar.GetMonth(date);
+
+    public static int GetShamsiDay(this DateTime date) => _persianCalendar.GetDayOfMonth(date);
+
+    public static string GetShamsiMonthName(this DateTime date)
+    {
+        var month = _persianCalendar.GetMonth(date);
+        return PersianMonthNames[month - 1];
+    }
+
+    public static string ToPersianDigits(this string input)
+    {
+        return input
+            .Replace("0", "۰").Replace("1", "۱").Replace("2", "۲")
+            .Replace("3", "۳").Replace("4", "۴").Replace("5", "۵")
+            .Replace("6", "۶").Replace("7", "۷").Replace("8", "۸")
+            .Replace("9", "۹");
+    }
+
+    public static string ToPersianDigits(this int number) => number.ToString().ToPersianDigits();
+
+    public static string ToPersianDigits(this long number) => number.ToString().ToPersianDigits();
+
+    public static string ToPersianDigits(this double number) => number.ToString().ToPersianDigits();
+
+    public static string ToPersianDigits(this decimal number) => number.ToString("#,##0.##").ToPersianDigits();
+
     public static string ToPersianName(this ServiceCategory category) => category switch
     {
         ServiceCategory.Videography => "فیلم‌برداری",
@@ -73,6 +129,6 @@ public static class EnumExtensions
 
     public static string FormatPrice(this decimal price)
     {
-        return $"{price:N0} تومان";
+        return $"{price:N0} تومان".ToPersianDigits();
     }
 }
